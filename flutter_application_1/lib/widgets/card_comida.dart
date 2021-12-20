62311bc49d9d109bafa7c8356295ec4c22695129
Povedajf <65924214+Povedajf@users.noreply.github.com>
2021-12-19 20:00:41 -0500

    trabajo

---
+++ flutter_application_1/lib/widgets/card_comida.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/proto_model.dart';
+
+class CardPlatos extends StatelessWidget {
+  const CardPlatos({Key? key, required this.model}) : super(key: key);
+  final FieldsProto model;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 9.0,
+      child: ListTile(
+
+        title: Text(model.nombrePlato!.stringValue.toString(),
+            style: Theme.of(context).textTheme.headline6),
+        subtitle: Text(
+            model.nombrePlato!.stringValue.toString(),
+            style: Theme.of(context).textTheme.subtitle2),
+        trailing: Text(
+            "Valor Plato: " +
+                model.valorPlato!.doubleValue.toString() +
+                " \$",
+            style: Theme.of(context).textTheme.headline6),
+      ),
+    );
+  }
+}
